dc822844e79e42b84ccebb813ddcd6b07cd20c3f
chaeyk <chaeyk@neowiz.com>
2020-10-29 14:58:08 +0900

    log firebase http req/res

--- src/main/java/net/chaeyk/istioleak/HomeController.java
+++ src/main/java/net/chaeyk/istioleak/HomeController.java
@@ -15,23 +15,28 @@ public class HomeController {
 
     private final FirebaseService firebaseService;
 
-    @GetMapping
-    public boolean home(@RequestParam(required = false, defaultValue = "f684qUeZTOSV1EJZeUbhHm:APA91bHCmMR19kHia-ZfS0r_TPZR-Kkm2Y2tF7AB4cCLQ5ZLdTK4iOV6e41MDctiOuZW0jPJV3AviztooCpbDJEAis-6a_ANWMRXT7nTjI0HrQ_11haTaMrbI7scFxtyp7nIX83dxhqe") String token,
-                        @RequestParam(required = false, defaultValue = "weather") String topic,
-                        @RequestParam(required = false, defaultValue = "1000") int count) throws FirebaseMessagingException {
+    @GetMapping("/")
+    public String home() {
+        return "1";
+    }
+
+    @GetMapping("/register")
+    public boolean register(@RequestParam(required = false, defaultValue = "f684qUeZTOSV1EJZeUbhHm:APA91bHCmMR19kHia-ZfS0r_TPZR-Kkm2Y2tF7AB4cCLQ5ZLdTK4iOV6e41MDctiOuZW0jPJV3AviztooCpbDJEAis-6a_ANWMRXT7nTjI0HrQ_11haTaMrbI7scFxtyp7nIX83dxhqe") String token,
+                            @RequestParam(required = false, defaultValue = "weather") String topic,
+                            @RequestParam(required = false, defaultValue = "1000") int count) throws FirebaseMessagingException {
+        boolean success = true;
+        for (int i = 0; i < count; i++)
+            success = firebaseService.register(token, topic) && success;
+        return success;
+    }
+
+    @GetMapping("/unregister")
+    public boolean unregister(@RequestParam(required = false, defaultValue = "f684qUeZTOSV1EJZeUbhHm:APA91bHCmMR19kHia-ZfS0r_TPZR-Kkm2Y2tF7AB4cCLQ5ZLdTK4iOV6e41MDctiOuZW0jPJV3AviztooCpbDJEAis-6a_ANWMRXT7nTjI0HrQ_11haTaMrbI7scFxtyp7nIX83dxhqe") String token,
+                              @RequestParam(required = false, defaultValue = "weather") String topic,
+                              @RequestParam(required = false, defaultValue = "1000") int count) throws FirebaseMessagingException {
         boolean success = true;
         for (int i = 0; i < count; i++)
             success = firebaseService.unregister(token, topic) && success;
         return success;
-    }
-
-    @GetMapping("/register/{topic}/{token}")
-    public boolean register(@PathVariable String topic, @PathVariable String token) throws FirebaseMessagingException {
-        return firebaseService.register(token, topic);
-    }
-
-    @GetMapping("/ver")
-    public String ver() {
-        return "2";
     }
 }
